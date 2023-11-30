fa4716e5f11adc96830f88b5f54189e532ffdf9f
Rhfialyndra <l>
2023-11-30 11:01:34 +0700

    implement register, login and logout

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,10 @@
 # urls.py
 from django.urls import path
-from .views import signup, login_view
+from .views import signup, login_view, logout_view
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
     path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
     # Add more URL patterns as needed
 ]
