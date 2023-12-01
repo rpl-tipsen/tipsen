90cc3082afeefdb6c91f2dd371b98a2be6b8fa8d
Rhfialyndra <l>
2023-12-01 21:28:00 +0700

    enable cors

--- tipsen/settings.py
+++ tipsen/settings.py
@@ -25,8 +25,13 @@ SECRET_KEY = 'django-insecure-f=#=4_(skom8*n*2r(#*+o%_ekq%c!o=sbxwtem31n7@=z!c01
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost", 'tipsen.monemone.site']
 
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3030',
+    'https://tipsen.monemone.site'
+]
 
 # Application definition
 
@@ -40,7 +45,8 @@ INSTALLED_APPS = [
     'authentication',
     'home',
     'userprofile',
-    'products'
+    'products',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -51,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 ROOT_URLCONF = 'tipsen.urls'
 
