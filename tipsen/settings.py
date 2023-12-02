7489bc3db8b430ad97a74374922d5f0d771eb125
Aushaaf Fadhilah Azzah <fadhilahazzah04@gmail.com>
2023-12-02 15:05:48 +0700

    try to fix csrf #2

--- tipsen/settings.py
+++ tipsen/settings.py
@@ -33,12 +33,21 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'https://tipsen.monemone.site'
 ]
+CSRF_ALLOWED_ORIGINS = [
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+    'https://tipsen.monemone.site'
+]
 CORS_TRUSTED_ORIGINS = [
     'http://localhost:8000',
     'http://127.0.0.1:8000',
     'https://tipsen.monemone.site'
 ]
-
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+    'https://tipsen.monemone.site'
+]
 
 # Application definition
 
