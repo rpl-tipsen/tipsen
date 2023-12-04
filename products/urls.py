69efe021a99d57434bb40bdfe40349e08f4fe1b0
swusjask <89961456+swusjask@users.noreply.github.com>
2023-12-04 17:47:56 +0700

    kelar gak sih rpl sat set (#27)

--- products/urls.py
+++ products/urls.py
@@ -5,6 +5,9 @@ from .views import *
 urlpatterns = [
     path("", products, name="products"),
     path("<int:product_id>", product, name="product"),
-    path("order/<int:product_id>", order_product,
-         name="order_product"),
+    path("order/<int:product_id>", order_product, name="order_product"),
+    path("admin", admin_products, name="admin-products"),
+    path("admin/<int:product_id>", admin_product, name="admin-product"),
+    path("admin/create", admin_create, name="admin-create"),
+    path("admin/delete/<int:product_id>", admin_delete, name="admin-delete"),
 ]
--- products/views.py
+++ products/views.py
@@ -15,16 +15,14 @@ from tipsen.settings import SUPABASE
 
 
 BUCKET_NAME = "payment"
-sb = supabase.create_client(
-    supabase_url=SUPABASE["url"], supabase_key=SUPABASE["key"])
+BUCKET_NAME_2 = "product"
+sb = supabase.create_client(supabase_url=SUPABASE["url"], supabase_key=SUPABASE["key"])
 
 
 def products(request):
     products = Product.objects.all()
 
-    return render(
-        request, "products/index.html", context={"products": products}
-    )
+    return render(request, "products/index.html", context={"products": products})
 
 
 def product(request, product_id):
@@ -32,9 +30,81 @@ def product(request, product_id):
     if not product:
         return redirect("home")
 
-    return render(
-        request, "products/detail.html", context={"product": product}
-    )
+    return render(request, "products/detail.html", context={"product": product})
+
+
+@login_required
+def admin_products(request):
+    if not request.user.is_admin:
+        return redirect("home")
+
+    products = Product.objects.all()
+    return render(request, "products/admin-index.html", context={"products": products})
+
+
+@login_required
+def admin_product(request, product_id):
+    if not request.user.is_admin:
+        return redirect("home")
+
+    product = Product.objects.get(id=product_id)
+    if not product:
+        return redirect("home")
+
+    return render(request, "products/admin-detail.html", context={"product": product})
+
+
+@login_required
+def admin_create(request):
+    if not request.user.is_admin:
+        return redirect("home")
+    
+    if request.method == "POST":
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.cleaned_data["image"]
+
+            image_data = BufferedReader(image.file)
+            random_filename = str(uuid.uuid4()) + "_" + image.name
+
+            content_type, encoding = mimetypes.guess_type(image.name)
+            file_options = {"content-type": content_type} if content_type else {}
+
+            response = sb.storage.from_(BUCKET_NAME_2).upload(
+                path=random_filename, file=image_data, file_options=file_options
+            )
+
+            if response.status_code == 201 or response.status_code == 200:
+                image_link = f"{SUPABASE['url']}/storage/v1/object/public/{BUCKET_NAME_2}/{random_filename}"
+            else:
+                return "Failed to create payment"
+            
+            Product.objects.create(
+                name=request.POST["name"],
+                price=request.POST["price"],
+                description=request.POST["description"],
+                image_link=image_link,
+            ).save()
+
+            return render(request, "products/admin-create.html", context={"success": True})
+
+    return render(request, "products/admin-create.html")
+
+@login_required
+def admin_delete(request, product_id):
+    if not request.user.is_admin:
+        return redirect("home")
+    
+    product = Product.objects.get(id=product_id)
+    if not product:
+        return redirect("home")
+    
+    sb.storage.from_(BUCKET_NAME_2).remove(product.image_link.split("/")[-1])
+    product.delete()
+    
+    return redirect("admin-products")
+    
+
 
 
 @login_required
@@ -46,51 +116,60 @@ def order_product(request, product_id):
     addresses = Address.objects.all()
     banks = ["BCA", "Mandiri", "BNI"]
 
-    if request.method == 'POST':
+    if request.method == "POST":
         print(request.POST)
-        if request.POST.get('step') == 'first':
+        if request.POST.get("step") == "first":
             first_form = OrderFirstForm(request.POST)
             if first_form.is_valid():
-                address_id = first_form.cleaned_data['address_id']
+                address_id = first_form.cleaned_data["address_id"]
                 address = Address.objects.get(id=address_id)
                 if not address or address.user_id != request.user._wrapped.id:
                     return render(
                         request,
                         "products/form-before-payment.html",
                         context={
-                            "product": product, "addresses": addresses,
+                            "product": product,
+                            "addresses": addresses,
                             "error": True,
                             "message": f"couldn't find address with id {address_id}",
                         },
                     )
-                quantity = first_form.cleaned_data['quantity']
+                quantity = first_form.cleaned_data["quantity"]
                 total_price = product.price * quantity
                 print(total_price, quantity)
 
                 # Save order data in session
-                request.session['address_id'] = address_id
-                request.session['quantity'] = quantity
-                request.session['total_price'] = total_price
+                request.session["address_id"] = address_id
+                request.session["quantity"] = quantity
+                request.session["total_price"] = total_price
                 return render(
-                    request, "products/form-after-payment.html",
-                    context={"product": product, "addresses": addresses,
-                             "banks": banks}
+                    request,
+                    "products/form-after-payment.html",
+                    context={
+                        "product": product,
+                        "addresses": addresses,
+                        "banks": banks,
+                    },
                 )
             else:
                 print(first_form.errors)
                 return render(
-                    request, "products/form-before-payment.html",
-                    context={"product": product, "addresses": addresses,
-                             "form": first_form}
+                    request,
+                    "products/form-before-payment.html",
+                    context={
+                        "product": product,
+                        "addresses": addresses,
+                        "form": first_form,
+                    },
                 )
         else:
             second_form = OrderSecondForm(request.POST)
             if second_form.is_valid():
                 try:
                     # Retrieve order data from session
-                    address_id = request.session.get('address_id')
-                    quantity = request.session.get('quantity')
-                    total_price = request.session.get('total_price')
+                    address_id = request.session.get("address_id")
+                    quantity = request.session.get("quantity")
+                    total_price = request.session.get("total_price")
 
                     address = Address.objects.get(id=address_id)
                     if not address or address.user_id != request.user._wrapped.id:
@@ -98,7 +177,8 @@ def order_product(request, product_id):
                             request,
                             "products/form-before-payment.html",
                             context={
-                                "product": product, "addresses": addresses,
+                                "product": product,
+                                "addresses": addresses,
                                 "error": True,
                                 "message": f"couldn't find address with id {address_id}",
                             },
@@ -114,33 +194,44 @@ def order_product(request, product_id):
                         address=address,
                         subtotal=total_price,
                         user=request.user._wrapped,
-                        payment=payment
+                        payment=payment,
                     ).save()
 
-                    del request.session['address_id']
-                    del request.session['quantity']
-                    del request.session['total_price']
+                    del request.session["address_id"]
+                    del request.session["quantity"]
+                    del request.session["total_price"]
 
                     # TODO: ganti redirect ke "pesanan saya"
-                    return redirect('home')
+                    return redirect("home")
                 except Exception as e:
                     return render(
-                        request, "products/form-after-payment.html",
-                        context={"product": product, "addresses": addresses,
-                                 "exception_error": {e}, "banks": banks}
+                        request,
+                        "products/form-after-payment.html",
+                        context={
+                            "product": product,
+                            "addresses": addresses,
+                            "exception_error": {e},
+                            "banks": banks,
+                        },
                     )
             else:
                 print(second_form.errors)
                 return render(
-                    request, "products/form-after-payment.html",
-                    context={"product": product, "addresses": addresses,
-                             "form": OrderSecondForm, "banks": banks}
+                    request,
+                    "products/form-after-payment.html",
+                    context={
+                        "product": product,
+                        "addresses": addresses,
+                        "form": OrderSecondForm,
+                        "banks": banks,
+                    },
                 )
 
     return render(
-        request, "products/form-before-payment.html", context={"product": product, "addresses": addresses}
+        request,
+        "products/form-before-payment.html",
+        context={"product": product, "addresses": addresses},
     )
-
 
 def handle_payment_creation(request):
     form = ImageForm(request.POST, request.FILES)
@@ -165,12 +256,12 @@ def handle_payment_creation(request):
         else:
             return "Failed to create payment"
 
-        total_price = request.session.get('total_price')
+        total_price = request.session.get("total_price")
 
         payment = Payment.objects.create(
-            payment_reference_number=request.POST['payment_reference_number'],
+            payment_reference_number=request.POST["payment_reference_number"],
             jumlah=total_price,
-            bank=request.POST['bank'],
+            bank=request.POST["bank"],
             image_link=image_link,
         )
 
