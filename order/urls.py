f1114b685162597289ec26c4f95fdbae123084f2
ferupk <92253739+ferupk@users.noreply.github.com>
2023-12-04 19:34:55 +0700

    implement initial order management (#28)

--- order/urls.py
+++ order/urls.py
@@ -1,8 +1,8 @@
 # urls.py
 from django.urls import path
-from .views import special_order
+from .views import special_order, manage_orders
 
 urlpatterns = [
     path('special_request/', special_order, name='special_order'),
-    # Add more URL patterns as needed
+    path('admin/', manage_orders, name='manage_orders')
 ]
